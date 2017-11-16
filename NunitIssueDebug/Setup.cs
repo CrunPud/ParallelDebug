195d6d818e80b52e27a4997ae1f9d149ae9e5dc8
Ian Goodman <mcrae.goodman@gmail.com>
2017-11-16 16:43:37 -0700

    added a second setup/teardown

---
+++ NunitIssueDebug/Setup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+
+namespace NunitIssueDebug
+{
+    [SetUpFixture]
+    public class Setup
+    {
+        [SetUp]
+        public void TestSetup()
+        {
+            Console.WriteLine("Setting up the test");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Thread.Sleep(500);
+            Console.WriteLine("Tearing down the test");
+        }
+    }
+}
